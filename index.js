157244c67531319c0df4bef1ccefb09de855a761
Roman <spriggan1992@gmail.com>
2019-10-01 21:22:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPatter = [];
+
+
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPatter.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
+
+
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColor);
+  animatePress(userChosenColor);
+
+}
+
+function playSound(name) {
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+
+  setTimeout(function () {
+  $("#"+ currentColor).removeClass("pressed");
+  }, 100);
+}
